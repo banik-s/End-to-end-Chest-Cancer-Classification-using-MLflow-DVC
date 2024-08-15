22b0db6942e72f9487f020006f0a7fb7cbcc793e
banik-s <swarnendu.banik28@gmail.com>
2024-08-16 02:03:55 +0530

    Add files via upload

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -59,16 +59,14 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chest-CT-Scan-data")
-        create_directories([
-            Path(training.root_dir)
-        ])
+        training_data = Path(r'C:\Users\Swarnendu\Desktop\End-to-end-Chest-Cancer-Classification-using-MLflow-DVC\research\extracted_files\Chest-CT-Scan-data')
+        create_directories([Path(training.root_dir)])
 
         training_config = TrainingConfig(
             root_dir=Path(training.root_dir),
             trained_model_path=Path(training.trained_model_path),
             updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
-            training_data=Path(training_data),
+            training_data=training_data,
             params_epochs=params.EPOCHS,
             params_batch_size=params.BATCH_SIZE,
             params_is_augmentation=params.AUGMENTATION,
